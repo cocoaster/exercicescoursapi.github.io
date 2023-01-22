39f0ed39dde6ed86caf237365e8d8c0b153a48b5
coralie montalbano <montalbanocoralie@gmail.com>
2023-01-22 17:11:37 +0100

    c4

--- cours/index.js
+++ cours/index.js
@@ -7,3 +7,7 @@ req.onload = reqListener;
 // req.open("get", "data.json", true);
 req.open("get", "https://api.blablagues.net/?rub=blagues", true);
 req.send();
+
+fetch("monlien", "object d'options")
+  .then((reponse) => {})
+  .catch((err) => console.log(err));
