70c12443cb99a4c1ddd4f22c3c1cb4af32e28a8d
coralie montalbano <montalbanocoralie@gmail.com>
2023-01-23 09:06:37 +0100

    c11

--- user-app/index.js
+++ user-app/index.js
@@ -1,9 +1,28 @@
 let userData = [];
 
-const fetchUser = () => {
-  fetch("https://randomuser.me/api/?results=24")
+const fetchUser = async () => {
+  await fetch("https://randomuser.me/api/?results=24")
     .then((res) => res.json())
-    .then((data) => (userData = data.results));
-  console.log(userData);
+    .then((data) => (userData = data.results))
+    .then(() => console.log(userData));
 };
-fetchUser();
+
+const userDisplay = async () => {
+  await fetchUser();
+
+  document.body.innerHTML = userData
+    .map(
+      (user) =>
+        `
+    <div class="card">
+        <img src=${user.picture.large} alt="photo de ${user.name.last}">
+        <h3>${user.name.first}</h3>
+        <p>${user.location.city}</p>
+        <em> Membre depuis : ${user.registrated.date} jours</em>
+    </div>
+    `
+    )
+    .join("");
+};
+
+userDisplay();
