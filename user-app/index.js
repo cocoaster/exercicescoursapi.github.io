491e34c2de6e493f9450363b8b8b2ffa1a6fcdff
coralie montalbano <montalbanocoralie@gmail.com>
2023-01-23 08:13:57 +0100

    c10

--- user-app/index.js
+++ user-app/index.js
@@ -1,1 +1,9 @@
-fetch('https://randomuser.me/api/?results=24').then((res) => console.log(res));
+let userData = [];
+
+const fetchUser = () => {
+  fetch("https://randomuser.me/api/?results=24")
+    .then((res) => res.json())
+    .then((data) => (userData = data.results));
+  console.log(userData);
+};
+fetchUser();
