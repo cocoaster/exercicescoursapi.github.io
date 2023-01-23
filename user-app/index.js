b009a86a2533abd5229564d3693334d497e70af8
coralie montalbano <montalbanocoralie@gmail.com>
2023-01-23 09:19:14 +0100

    c12

--- user-app/index.js
+++ user-app/index.js
@@ -18,7 +18,7 @@ const userDisplay = async () => {
         <img src=${user.picture.large} alt="photo de ${user.name.last}">
         <h3>${user.name.first}</h3>
         <p>${user.location.city}</p>
-        <em> Membre depuis : ${user.registrated.date} jours</em>
+        <em> Membre depuis : ${user.registered.date} jours</em>
     </div>
     `
     )
